cae97cb5d17138a859d4245bf6b840c3d3855771
dejk-alg <yaroslav@taranov.net>
2020-03-28 13:57:19 +0200

    backend testing example in main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from backend import IntegralAwarenessFactors
 
 def run():
     factors = IntegralAwarenessFactors.default_instance()
-    factors.plot(3, 4, range(0, 200))
+    factors.plot(2, 2, range(25))
+    print(factors.critical_time_range(2, 2))
 
 
 if __name__ == '__main__':
