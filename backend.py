2c11aaa48fd4e23d241e024f3d5fc41cd0c3a0cb
dejk-alg <yaroslav@taranov.net>
2020-03-28 18:13:32 +0200

    Update

--- backend.py
+++ backend.py
@@ -17,21 +17,25 @@ def lazy_wrapper(func: Callable):
 class IntegralAwareness:
     def __init__(
             self, alphas_0: np.ndarray,
-            completeness_0: np.ndarray, credibility_0: np.ndarray, timeliness_0: np.ndarray):
+            completeness_0: np.ndarray, credibility_0: np.ndarray, timeliness_0: np.ndarray,
+            max_prob: float, limit_t: Optional[int] = None):
         self.alphas_0 = alphas_0
         self.completeness_0 = completeness_0
         self.credibility_0 = credibility_0
         self.timeliness_0 = timeliness_0
         self.cached_values = defaultdict(dict)
         self.alphas = self.betas = self.gammas = None
+        self.situations_amount = self.factors_amount = None
         self.set_coeffs()
+        self.max_prob = max_prob
+        self.limit_t = limit_t
 
     def valid_factors(self, i):
         return np.where(self.alphas_0[i] != -1)
 
     @classmethod
-    def default_instance(cls):
-        return cls(**cls.default_values())
+    def default_instance(cls, max_prob: float, limit_t: Optional[int] = None):
+        return cls(**cls.default_values(), max_prob=max_prob, limit_t=limit_t)
 
     @staticmethod
     def parse_values_dict(string_dict):
@@ -79,6 +83,7 @@ class IntegralAwareness:
         self.alphas = 1 + (1 + self.alphas_0) * (1 + self.completeness_0 * self.credibility_0 * self.timeliness_0)
         self.betas = 1 + (1 + self.alphas_0 * self.credibility_0)
         self.gammas = (1 + 0.5 * self.betas * self.alphas_0 ** 2 * self.timeliness_0) ** 2
+        self.situations_amount, self.factors_amount = self.alphas.shape
 
     def completeness_factor(self, t: int):
         return 1 - (1 - self.completeness_0) * np.exp(-1e-4 * ((self.alphas + self.gammas) * t) ** 2)
@@ -111,19 +116,19 @@ class IntegralAwareness:
 
         return np.array([factor(t)[i, j] for t in time_range])
 
-    def find_best_prob(self, i: int, j: int, limit_t: Optional[int] = None):
+    def find_best_prob(self, i: int, j: int):
         prev_prob = 1
         for t in count():
             crit_prob_unscaled = self.critical_probability(t)[i, j]
             if crit_prob_unscaled > prev_prob:
                 return t - 1, prev_prob
             prev_prob = crit_prob_unscaled
-            if limit_t is not None and t > limit_t:
+            if self.limit_t is not None and t > self.limit_t:
                 return None
 
-    def critical_time_range_per_factor(self, i: int, j: int, max_prob: float = 0.7, limit_t: Optional[int] = None):
-        best_t, best_prob = self.find_best_prob(i, j, limit_t)
-        scaled_max_prob = max_prob * (1 - best_prob) + best_prob
+    def critical_time_range_per_factor(self, i: int, j: int):
+        best_t, best_prob = self.find_best_prob(i, j)
+        scaled_max_prob = self.max_prob * (1 - best_prob) + best_prob
         min_t = None
         for t in count():
             if min_t is None and self.critical_probability(t)[i, j] < scaled_max_prob:
@@ -131,19 +136,18 @@ class IntegralAwareness:
             if min_t is not None and self.critical_probability(t)[i, j] > scaled_max_prob:
                 max_t = t - 1
                 return min_t, max_t
-            if limit_t is not None and t >=\
-                    limit_t:
+            if self.limit_t is not None and t >= self.limit_t:
                 if min_t is None:
                     raise ValueError
-                max_t = limit_t
+                max_t = self.limit_t
                 return min_t, max_t
 
-    def critical_time_range(self, i: int, max_prob: float = 0.7, limit_t: Optional[int] = None):
-        time_ranges = [self.critical_time_range_per_factor(i, j, max_prob, limit_t) for j in self.valid_factors(i)]
+    def critical_time_range(self, i: int):
+        time_ranges = [self.critical_time_range_per_factor(i, j,) for j in self.valid_factors(i)]
         return max(map(lambda x: x[0], time_ranges)), min(map(lambda x: x[1], time_ranges))
 
-    def classify_situation(self, i: int, max_prob: float, limit_t):
-        time_range = self.critical_time_range(i, max_prob, limit_t)
+    def classify_situation(self, i: int):
+        time_range = self.critical_time_range(i)
         time = 20
         if time <= time_range[0]:
             return 'A1 - особливо небезпечна ситуація'
@@ -151,6 +155,11 @@ class IntegralAwareness:
             return 'A2 - потенційно небезпечна ситуація'
         else:
             return 'A3 - майже безпечна ситуація'
+
+    def classification_df(self):
+        classification = {i: self.classify_situation(i) for i in range(self.situations_amount)}
+        classification = DataFrame(classification)
+        return classification
 
     def create_timeseries_df(self, i: int, j: int, time_range: Iterable):
         timeseries = {name: self.timeseries(name, i, j, time_range)
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from backend import IntegralAwarenessFactors
+from backend import IntegralAwareness
+
+
+def get_outputs(i: int, j: int, time_start: int, time_stop: int, time_step: int, max_prob: float):
+    factors = IntegralAwareness.default_instance(max_prob=max_prob, limit_t=time_stop)
+    time_range = range(time_start, time_stop, time_step)
+    i -= 1
+    j -= 1
+    factors.plot(i, j, time_range)
+    return {
+        'plot_dict': factors.get_plot_dict(i, j, time_range),
+        'critical_time_range': factors.critical_time_range(i, j, time_range, max_prob),
+        'timeseries_df': factors.create_timeseries_df()
+    }
 
 
 def run():
-    factors = IntegralAwarenessFactors.default_instance()
-    factors.plot(2, 2, range(25))
-    print(factors.critical_time_range(2, 2))
+    print(get_outputs(3, 3, 0, 70, 1, 0.7))
 
 
 if __name__ == '__main__':
