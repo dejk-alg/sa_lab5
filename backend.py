395b234f50aef592fffe8cbc2c309d8adf047f73
dejk-alg <yaroslav@taranov.net>
2020-05-11 23:32:00 +0300

    Lab7 version 1.0

--- backend.py
+++ backend.py
@@ -1,195 +1,190 @@
-from typing import Callable, Optional, Iterable
 from collections import defaultdict
-from itertools import count
 import numpy as np
-import matplotlib.pyplot as plt
-from pandas import DataFrame
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import patches
+import networkx as nx
+
+np.set_printoptions(precision=3)
 
 
-def lazy_wrapper(func: Callable):
-    def lazy_func(self, t: int):
-        if t not in self.cached_values[func].keys():
-            self.cached_values[func][t] = func(self, t)
-        return self.cached_values[func][t]
-    return lazy_func
+class GraphProcessor:
+    def __init__(self):
+        self.url = 'https://docs.google.com/spreadsheets/d/1WM2UkpA-0DqowowpkF7mck6d1tyFJNoWaSM7YsG6vRc' \
+                   '/export?gid={gid}&format=csv'
+        self.df = self.read_df()
+        self.start_values = self.read_start_values()
+        self.graph = self.create_graph()
+        self.scenario_len = 10
 
+        self.scenario_impulses = defaultdict(dict)
+        self.impulses = {}
+        self.base_values = None
+        self.scenario_values = None
 
-class IntegralAwareness:
-    def __init__(
-            self, alphas_0: np.ndarray,
-            completeness_0: np.ndarray, credibility_0: np.ndarray, timeliness_0: np.ndarray,
-            max_prob: float, limit_t: Optional[int] = None):
-        self.alphas_0 = alphas_0
-        self.completeness_0 = completeness_0
-        self.credibility_0 = credibility_0
-        self.timeliness_0 = timeliness_0
-        self.cached_values = defaultdict(dict)
-        self.alphas = self.betas = self.gammas = None
-        self.situations_amount = self.factors_amount = None
-        self.set_coeffs()
-        self.max_prob = max_prob
-        self.limit_t = limit_t
+        self.cur_time = 0
+        self.process_scenario()
 
-    def valid_factors(self, i):
-        return np.where(self.alphas_0[i] != -1)[0]
+    def reset(self):
+        self.graph = self.create_graph()
 
-    @classmethod
-    def default_instance(cls, max_prob: float, limit_t: Optional[int] = None):
-        return cls(**cls.default_values(), max_prob=max_prob, limit_t=limit_t)
+    def reset_scenario(self):
+        if self.base_values is not None:
+            self.set_values(self.base_values)
+
+        self.impulses = {}
+        self.scenario_impulses = defaultdict(dict)
+
+        self.base_values = None
+        self.scenario_values = None
+        self.cur_time = 0
+        self.process_scenario()
+
+    def read_df(self):
+        return pd.read_csv(self.url.format(gid=0), index_col=0).replace('-', 0).fillna(0).rename_axis(
+            'Фактор', axis=0).astype(np.float)
+
+    def read_start_values(self):
+        return pd.read_csv(self.url.format(gid=592692655), index_col=0).replace('-', 0).fillna(0).rename_axis(
+            'Фактор', axis=0).astype(np.float).transpose()
+
+    def get_factor_names(self):
+        return self.df.columns
+
+    def get_matrix(self):
+        return self.df.to_numpy()
+
+    def create_graph(self):
+        graph = nx.DiGraph()
+        for name, value in self.start_values['Початкове значення'].items():
+            graph.add_node(name, value=value)
+        for col_name, column in self.df.items():
+            for row_name, weight in column.iteritems():
+                if weight != 0:
+                    graph.add_edge(row_name, col_name, weight=weight)
+        return graph
+
+    def plot_graph_on_figure(self, figure=None):
+        if figure is None:
+            figure = plt.figure()
+        figure.clear()
+        figure.set_facecolor('w')
+        ax = figure.add_axes((0, 0, 1, 1))
+        pos = patches.RegularPolygon(xy=(0.5, 0.5), radius=0.5, numVertices=self.graph.number_of_nodes()).get_verts()
+        pos = {node: coord for node, coord in zip(self.graph.nodes.keys(), pos)}
+        nx.draw_networkx(
+            self.graph, pos=pos, ax=ax,
+            with_labels=True,
+            font_weight='bold',
+            node_shape='H', node_color=[[0.8, 0.3, 0.1]],
+            edge_color=[[0.3, 0., 0.9]],
+            width=[abs(weight) * 3 for weight in nx.get_edge_attributes(self.graph, 'weight').values()],
+            font_size=11, font_family='calibri',
+            node_size=[data['value'] * 100 for node, data in self.graph.nodes.items()])
+        ax.set_axis_off()
+        return figure
+
+    def add_node(self, node, value):
+        self.graph.add_node(node, value=value)
+        self.df[node] = 0
+        self.df.loc[node] = 0
+
+    def add_edge(self, node1, node2, weight):
+        self.graph.add_edge(node1, node2, weight=weight)
+        self.df.loc[node1, node2] = weight
+
+    def delete_edge(self, node1, node2):
+        self.graph.remove_edge(node1, node2)
+        self.df.loc[node1, node2] = 0
+
+    def delete_node(self, node):
+        self.graph.remove_node(node)
+        self.df = self.df.drop(node, axis=0)
+        self.df = self.df.drop(node, axis=1)
+
+    def get_values(self):
+        return {k: v for k, v in self.graph.nodes(data='value')}
+
+    def set_values(self, values):
+        nx.set_node_attributes(self.graph, values, name='value')
+
+    def get_even_cycles(self):
+        return [cycle for cycle in nx.simple_cycles(self.graph) if len(cycle) % 2 == 0]
+
+    def add_impulse(self, node, time, value):
+        self.scenario_impulses[time][node] = value
+
+    def process_impulse(self, node, value):
+        self.graph.nodes[node]['value'] += value
+
+    def time_step(self):
+        self.impulses.update(self.scenario_impulses[self.cur_time])
+        new_impulses = defaultdict(lambda: 0)
+        for node, value in self.impulses.items():
+            self.process_impulse(node, value)
+            for neighbor in self.graph.neighbors(node):
+                new_impulses[neighbor] += value * self.graph.edges[node, neighbor]['weight']
+        self.impulses = dict(new_impulses)
+        self.cur_time += 1
+
+    def process_scenario(self):
+        self.start_values = self.get_values()
+        self.scenario_values = []
+        for step_ind in range(self.scenario_len):
+            self.time_step()
+            self.scenario_values.append(self.get_values())
+        self.set_values(self.start_values)
+        self.cur_time = 0
+
+    def plot_scenario(self, figure=None):
+        if figure is None:
+            figure = plt.figure()
+        figure.clear()
+        legend = []
+        for node in self.graph.nodes:
+            plt.plot([values[node] for values in self.scenario_values])
+            legend.append(node)
+        ax = figure.gca()
+        ax.legend(legend)
+        return figure
+
+    def get_node_dynamics(self, node):
+        return [step_values[node] for step_values in self.scenario_values]
+
+    def set_step(self, step):
+        self.set_values(self.scenario_values[step])
+
+    def structural_stability(self):
+        even_cycles = self.get_even_cycles()
+        if even_cycles:
+            return 'Система не є структурно стійкою, наявні парні цикли:\n' + '\n'.join(' -> '.join(cycle)
+                                                                                       for cycle in even_cycles)
+        else:
+            return 'Система структурно стійка'
 
     @staticmethod
-    def parse_values_dict(string_dict):
-        return {
-            name: np.stack([np.fromstring(line, dtype=float, sep=' ')
-                            for line in inp_string.replace('-', '-1.').split('\n') if line.strip()], axis=0)
-            for name, inp_string in string_dict.items()}
+    def output_complex(number):
+        if number == 0:
+            return '0'
+        output_string = ''
+        if number.real:
+            output_string += f'{number.real:.2}'
+        if number.imag:
+            output_string += ' + ' if number.imag > 0 and number.real else ' - '
+            output_string += f'{np.abs(number.imag):.2}i'
+        return output_string
 
-    @staticmethod
-    def default_values():
-        text_dict = {
-            'alphas_0':
-                '''
-                0.6 0.5 0.4 0.55 - 0.755 0.45
-                - - 0.7 0.35 - - 0.7
-                0.65 - 0.8 0.65 0.7 0.65 0.7
-                - - 0.4 0.55 0.45 0.85 -
-                ''',
-            'completeness_0':
-                '''
-                0.65 0.55 0.8 0.45 - 0.7 0.75
-                - - 0.5 0.8 - - 0.45
-                0.45 - 0.6 0.5 0.6 0.45 0.45
-                - - 0.7 0.7 0.4 0.35 -
-                ''',
-            'credibility_0':
-                '''
-                0.3 0.54 0.5 0.4 - 0.5 0.4
-                - - 0.65 0.25 - - 0.5
-                0.35 - 0.35 0.4 0.2 0.45 0.3
-                - - 0.65 0.5 0.65 0.3 -
-                ''',
-            'timeliness_0':
-                '''
-                0.7 0.8 0.4 0.6 - 0.85 0.5
-                - - 0.5 0.4 - - 0.4
-                0.25 - 0.76 0.45 0.3 0.5 0.6
-                - - 0.5 0.5 0.6 0.3 -
-                '''
-
-        }
-        return IntegralAwareness.parse_values_dict(text_dict)
-
-    def set_coeffs(self):
-        self.alphas = 1 + (1 + self.alphas_0) * (1 + self.completeness_0 * self.credibility_0 * self.timeliness_0)
-        self.betas = 1 + (1 + self.alphas_0 * self.credibility_0)
-        self.gammas = (1 + 0.5 * self.betas * self.alphas_0 ** 2 * self.timeliness_0) ** 2
-        self.situations_amount, self.factors_amount = self.alphas.shape
-
-    def completeness_factor(self, t: int):
-        return 1 - (1 - self.completeness_0) * np.exp(-1e-4 * ((self.alphas + self.gammas) * t) ** 2)
-
-    def credibility_factor(self, t: int):
-        return 1 - (1 - self.credibility_0) * np.exp(-1e-4 * ((self.alphas + self.gammas) * t) ** 2)
-
-    def timeliness_factor(self, t: int):
-        return self.timeliness_0 * np.exp(-1e-2 * self.betas * t)
-
-    def awareness_factor(self, t: int) -> np.ndarray:
-        return np.prod(
-            [fact(t) for fact in (self.completeness_factor, self.credibility_factor, self.timeliness_factor)], axis=0)
-
-    def critical_probability(self, t: int) -> np.ndarray:
-        return 1 - np.log(1 + self.alphas * self.awareness_factor(t))
-
-    for attr_name in (
-            'completeness_factor', 'credibility_factor', 'timeliness_factor', 'awareness_factor',
-            'critical_probability'):
-        locals()[attr_name] = lazy_wrapper(locals()[attr_name])
-
-    def timeseries(self, name: str, i: int, j: int, time_range: Iterable) -> np.ndarray:
-        if name in ('completeness', 'credibility', 'timeliness', 'awareness'):
-            factor = getattr(self, f'{name}_factor')
-        elif name == 'critical_probability':
-            factor = getattr(self, name)
+    def impulse_stability(self):
+        eigen_values = np.linalg.eig(self.get_matrix())[0]
+        max_value = np.abs(eigen_values).max()
+        output_string = 'Власні числа: ' + ', '.join(self.output_complex(val) for val in set(eigen_values))
+        if max_value < 1:
+            output_string += '\nСистема стійка за початковим значенням'
+        if max_value <= 1:
+            output_string += '\nСистема стійка за збуренням'
         else:
-            raise ValueError(f'timeseries name {name} not recognized')
+            output_string += '\nСистема не є чисельно стійкою'
+        return output_string
 
-        return np.array([factor(t)[i, j] for t in time_range])
-
-    def find_best_prob(self, i: int, j: int):
-        prev_prob = 1
-        for t in count():
-            crit_prob_unscaled = self.critical_probability(t)[i, j]
-            if crit_prob_unscaled > prev_prob:
-                return t - 1, prev_prob
-            prev_prob = crit_prob_unscaled
-            if self.limit_t is not None and t > self.limit_t:
-                return None
-
-    def critical_time_range_per_factor(self, i: int, j: int):
-        best_t, best_prob = self.find_best_prob(i, j)
-        scaled_max_prob = self.max_prob * (1 - best_prob) + best_prob
-        min_t = None
-        for t in count():
-            if min_t is None and self.critical_probability(t)[i, j] < scaled_max_prob:
-                min_t = t
-            if min_t is not None and self.critical_probability(t)[i, j] > scaled_max_prob:
-                max_t = t - 1
-                return min_t, max_t
-            if self.limit_t is not None and t >= self.limit_t:
-                if min_t is None:
-                    raise ValueError
-                max_t = self.limit_t
-                return min_t, max_t
-
-    def critical_time_range(self, i: int):
-        time_ranges = [self.critical_time_range_per_factor(i, j) for j in self.valid_factors(i)]
-        return max(map(lambda x: x[0], time_ranges)), min(map(lambda x: x[1], time_ranges))
-
-    def classify_situation(self, i: int):
-        time_range = self.critical_time_range(i)
-        time = sum(self.critical_time_range(i)[1] for i in range(self.situations_amount)) / self.situations_amount * 0.8
-        if time <= time_range[0]:
-            return 'A1 - особливо небезпечна ситуація'
-        elif time <= time_range[1]:
-            return 'A2 - потенційно небезпечна ситуація'
-        else:
-            return 'A3 - майже безпечна ситуація'
-
-    def classification_df(self):
-        classification = DataFrame(
-            {'Клас небезпечної ситуації': [self.classify_situation(i) for i in range(self.situations_amount)]},
-            index=range(1, 1 + self.situations_amount))
-        return classification
-
-    def create_timeseries_df(self, i: int, j: int, time_range: Iterable):
-        timeseries = {name: self.timeseries(name, i, j, time_range)
-                      for name in ('completeness', 'credibility', 'timeliness', 'awareness')}
-        timeseries.update({'time': list(time_range)})
-        timeseries['values'] = {'critical_probability': self.timeseries('critical_probability', i, j, time_range)}
-        timeseries = DataFrame(timeseries)
-        return timeseries
-
-    def get_plot_dict(self, i: int, j: int, time_range: Iterable):
-        plot_dict = {f'{name}_factor': self.timeseries(name, i, j, time_range)
-                     for name in ('completeness', 'credibility', 'timeliness', 'awareness')}
-        plot_dict['time'] = list(time_range)
-        return plot_dict
-
-    def plot_iter(self, i: int, j: int, time_range: list, window_size: int = 10):
-        return (self.create_timeseries_df(i, j, range(time_range[0], finish, time_range[1] - time_range[0]))
-                for finish in list(range(time_range[0], time_range[-1], window_size)) + [time_range[-1]])
-
-    def plot(self, i: int, j: int, time_range: Iterable):
-        fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True)
-        for ax in axs[:, -1]:
-            ax.set_xlabel('time')
-        for name, ax, color in zip(
-                ('completeness', 'credibility', 'timeliness', 'awareness'), axs.flatten(), ('r', 'g', 'b', 'y')):
-            ax.plot(time_range, self.timeseries(name, i, j, time_range), color)
-            ax.set_ylabel(f'{name} factor')
-            ax.set_ylim((-0.1, 1.1))
-
-        fig.tight_layout()
-        plt.show()
-        plt.close(fig)
+    def graph_info(self):
+        return self.structural_stability() + '\n\n' + self.impulse_stability()
--- main.py
+++ main.py
@@ -1,39 +1,40 @@
-def backend(params):
-    return None
-
-
-import numpy as np
-
-import pyqtgraph as pg
 import sys
-#import qdarkstyle
+import qdarkstyle
 
-from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QSpinBox, QTabWidget, QApplication, QTextBrowser, QLineEdit, QTableView
-from PyQt5.QtCore import Qt, QAbstractTableModel
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QHeaderView, QSpinBox, QTabWidget, QApplication, \
+    QTextBrowser, QLineEdit, QTableView, QComboBox, QLabel
+from PyQt5.QtCore import Qt, QAbstractTableModel, pyqtSlot
+
+from backend import GraphProcessor
+
+import matplotlib
+matplotlib.use("Qt5Agg")
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 class TableModel(QAbstractTableModel):
     def __init__(self, data):
         super(TableModel, self).__init__()
-        data = np.array(data).tolist()
         self._data = data
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return self._data[index.row()][index.column()]
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
 
     def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
+        return self._data.shape[0]
 
     def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        return len(self._data[0])
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
 
 
 def connect(obj, func):
@@ -44,106 +45,199 @@ def connect(obj, func):
     return obj
 
 
+class QTabWidgetFixed(QTabWidget):
+    def showEvent(self, a0):
+        try:
+            return super().showEvent(a0)
+        finally:
+            if Qt.WindowState == Qt.WindowMaximized:
+                self.setFixedSize(self.size())
+                # pass
+
+
 class MainWindow(QWidget):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
+        self.graph_processor = GraphProcessor()
         self.__initUI__()
 
         self.__stop_calc = False
 
     def __initUI__(self):
-        self.graph = pg.PlotWidget()
+        self.figure = self.graph_processor.plot_graph_on_figure()
+        self.graph_canvas = FigureCanvas(self.figure)
+
+        self.plot_figure = self.graph_processor.plot_scenario()
+        self.plot_canvas = FigureCanvas(self.plot_figure)
+
         self.table = QTableView()
-        grapg_and_table = QTabWidget()
-        grapg_and_table.setMinimumHeight(400)
-        grapg_and_table.setMinimumWidth(700)
-        grapg_and_table.addTab(self.graph, "Graph")
-        grapg_and_table.addTab(self.table, 'table')
+        self.table.setModel(TableModel(self.graph_processor.df))
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
 
-        self.new_scenario_input = QLineEdit()
-        self.new_scenario_button = QPushButton('new_scenario')
-        self.new_scenario_button.clicked.connect(self.new_scenario_button_func)
-        self.restore_default_values_button = QPushButton('restore_default_values')
-        self.restore_default_values_button.clicked.connect(self.restore_default_values_func)
+        self.data_tabs = QTabWidgetFixed()
+        self.data_tabs.setMinimumHeight(400)
+        self.data_tabs.setMinimumWidth(700)
+        self.data_tabs.addTab(self.graph_canvas, 'Граф')
+        self.data_tabs.addTab(self.table, 'Таблиця')
+        self.data_tabs.addTab(self.plot_canvas, 'Імпульсне моделювання')
 
-        self.node_name_input_1 = QLineEdit()
-        self.node_name_input_2 = QLineEdit()
-        self.node_name_input_3 = QLineEdit()
-        self.bound_value_input = QLineEdit()
+        self.scenario_len_input = QLineEdit()
+        self.scenario_node = QComboBox()
+        self.impulse_input = QLineEdit()
+        self.impulse_time_input = QLineEdit()
+        self.add_impulse_button = QPushButton('Додати імпульс')
+        self.add_impulse_button.clicked.connect(self.add_impulse)
+        self.impulses_output = QLabel('')
 
-        self.add_node_button = QPushButton('add_node')
-        self.add_node_button.clicked.connect(self.add_node_func)
+        self.new_scenario_button = QPushButton('Обрахувати сценарій')
+        self.new_scenario_button.clicked.connect(self.new_scenario)
+        self.restore_button = QPushButton('Відновити початкові значення')
+        self.restore_button.clicked.connect(self.restore_default_values)
 
-        self.delete_node_button = QPushButton('delete_node')
-        self.delete_node_button.clicked.connect(self.delete_node_func)
+        self.new_node = QLineEdit()
+        self.node_value = QLineEdit()
 
-        self.apply_bound_value_button = QPushButton('apply_bound_value')
-        self.apply_bound_value_button.clicked.connect(self.apply_bound_value_func)
+        self.edge_node_1 = QComboBox()
+        self.edge_node_2 = QComboBox()
+
+        for node_box in (self.edge_node_1, self.edge_node_2, self.scenario_node):
+            node_box.addItems(self.graph_processor.get_factor_names())
+
+        self.edge_value = QLineEdit()
+
+        self.add_node_button = QPushButton('Додати фактор')
+        self.add_node_button.clicked.connect(self.add_node)
+
+        self.delete_node_button = QPushButton('Вилучити фактор')
+        self.delete_node_button.clicked.connect(self.delete_node)
+
+        self.add_edge_button = QPushButton("Додати зв'язок")
+        self.add_edge_button.clicked.connect(self.add_edge)
 
         self.text_output = QTextBrowser()
         self.text_output.setMinimumHeight(300)
         self.text_output.setMinimumWidth(400)
 
+        self._update_text()
+
         main_layout = QGridLayout()
         main_layout.setVerticalSpacing(20)
         main_layout.setHorizontalSpacing(20)
 
-        main_layout.addWidget(grapg_and_table, 0, 0, 3, 3)
+        main_layout.addWidget(self.data_tabs, 0, 0, 3, 3)
 
-        main_layout.addWidget(self.new_scenario_input, 0, 4,)
-        main_layout.addWidget(self.new_scenario_button, 1, 4,)
-        main_layout.addWidget(self.restore_default_values_button, 2, 4)
+        sub_layout = QGridLayout()
+        sub_layout.setVerticalSpacing(50)
+        sub_layout.addWidget(QLabel('Довжина сценарію'), 0, 0)
+        sub_layout.addWidget(self.scenario_len_input, 0, 1, 1, 2)
 
-        main_layout.addWidget(self.node_name_input_1, 3, 0)
-        main_layout.addWidget(self.add_node_button, 4, 0,)
-        main_layout.addWidget(self.delete_node_button, 5, 0,)
+        sub_layout.addWidget(self.scenario_node, 1, 0)
+        sub_layout.addWidget(self.impulse_input, 1, 1)
+        sub_layout.addWidget(self.impulse_time_input, 1, 2)
 
-        main_layout.addWidget(self.node_name_input_2, 3, 1,)
-        main_layout.addWidget(self.node_name_input_3, 3, 2,)
-        main_layout.addWidget(self.bound_value_input, 4, 1, 1, 2)
-        main_layout.addWidget(self.apply_bound_value_button, 5, 1, 1, 2)
+        sub_layout.addWidget(QLabel('Фактор'), 2, 0)
+        sub_layout.addWidget(QLabel('Значення'), 2, 1)
+        sub_layout.addWidget(QLabel('Час застосування'), 2, 2)
+
+        sub_layout.addWidget(self.add_impulse_button, 3, 0, 1, 3)
+        sub_layout.addWidget(self.impulses_output, 4, 0, 1, 3)
+        main_layout.addLayout(sub_layout, 0, 4, 3, 1, Qt.AlignCenter)
+
+        main_layout.addWidget(self.new_scenario_button, 1, 4)
+        main_layout.addWidget(self.restore_button, 2, 4)
+
+        sub_layout = QGridLayout()
+        sub_layout.addWidget(QLabel('Назва фактору'), 0, 0)
+        sub_layout.addWidget(self.new_node, 0, 1)
+        sub_layout.addWidget(QLabel('Початкове значення'), 1, 0)
+        sub_layout.addWidget(self.node_value, 1, 1)
+        main_layout.addLayout(sub_layout, 3, 0)
+
+        main_layout.addWidget(self.add_node_button, 4, 0)
+        main_layout.addWidget(self.delete_node_button, 5, 0)
+
+        sub_layout = QGridLayout()
+        sub_layout.addWidget(QLabel('Перший фактор'), 0, 0)
+        sub_layout.addWidget(self.edge_node_1, 0, 1)
+        sub_layout.addWidget(QLabel('Другий фактор'), 1, 0)
+        sub_layout.addWidget(self.edge_node_2, 1, 1)
+        sub_layout.addWidget(QLabel("Значення зв'язку"), 2, 0)
+        sub_layout.addWidget(self.edge_value, 2, 1)
+        main_layout.addLayout(sub_layout, 3, 1, 2, 1)
+
+        main_layout.addWidget(self.add_edge_button, 5, 1, 1, 2)
 
         main_layout.addWidget(self.text_output, 3, 4, 3, 1)
 
         self.setLayout(main_layout)
 
-    def get_params(self):
-        params = {
-            'new_scenario_input': self.new_scenario_input.text(),
-            'node_name_input_1': self.node_name_input_1.text(),
-            'node_name_input_2': self.node_name_input_2.text(),
-            'node_name_input_3': self.node_name_input_3.text(),
-            'bound_value_input': self.bound_value_input.text(),
-        }
-        return params
+    def _update(self):
+        self._update_graph()
+        self._update_text()
+
+    def _update_text(self):
+        self.text_output.clear()
+        self.text_output.append(self.graph_processor.graph_info())
+
+    def _update_graph(self):
+        self.graph_processor.plot_graph_on_figure(self.figure)
+        self.figure.canvas.draw()
+
+        self.graph_processor.plot_scenario(self.plot_figure)
+        self.plot_figure.canvas.draw()
+
+    def _read_scenario_input(self):
+        text = self.scenario_len_input.text()
+        self.graph_processor.scenario_impulses = [line.split(sep=',') for line in text.splitlines(keepends=False)]
 
     @pyqtSlot()
-    def new_scenario_button_func(self):
-        params = self.get_params()
-        self.text_output.setText(str(params))
-        return None
+    def new_scenario(self):
+        self.graph_processor.scenario_len = int(self.scenario_len_input.text())
+        self.graph_processor.process_scenario()
+        self._update_graph()
 
     @pyqtSlot()
-    def restore_default_values_func(self):
-        return None
+    def restore_default_values(self):
+        self.graph_processor.reset()
+        self._update()
 
     @pyqtSlot()
-    def add_node_func(self):
-        return None
+    def add_node(self):
+        self.graph_processor.add_node(self.new_node.text(), 0.5)
+        for node_box in (self.edge_node_1, self.edge_node_2, self.scenario_node):
+            node_box.addItem(self.new_node.text())
+        self.graph_processor.reset_scenario()
+        self._update()
 
     @pyqtSlot()
-    def delete_node_func(self):
-        return None
+    def delete_node(self):
+        self.graph_processor.delete_node(self.new_node.text())
+        for node_box in (self.edge_node_1, self.edge_node_2, self.scenario_node):
+            node_box.removeItem(self.edge_node_1.findText(self.new_node.text()))
+        self.graph_processor.reset_scenario()
+        self._update()
 
     @pyqtSlot()
-    def apply_bound_value_func(self):
-        return None
+    def add_edge(self):
+        value = float(self.edge_value.text())
+        if self.edge_value:
+            self.graph_processor.add_edge(self.edge_node_1.currentText(), self.edge_node_2.currentText(), value)
+        else:
+            self.graph_processor.delete_edge(self.edge_node_1.currentText(), self.edge_node_1.currentText())
+        self.graph_processor.reset_scenario()
+        self._update()
+
+    @pyqtSlot()
+    def add_impulse(self):
+        self.graph_processor.add_impulse(
+            self.scenario_node.currentText(),
+            float(self.impulse_input.text()),
+            int(self.impulse_time_input.text()))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    #app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
     main = MainWindow()
-    main.show()
+    main.showMaximized()
     sys.exit(app.exec_())
